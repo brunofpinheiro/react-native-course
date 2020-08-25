4b6e7249cd741cb76f37ca7997bdca4a8b6feaff
Bruno Pinheiro <bruno.faria.pinheiro@gmail.com>
2020-08-24 21:37:50 -0300

    aula 4

--- AprendaReactNative/src/components/App.js
+++ AprendaReactNative/src/components/App.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
 import Welcome from './Welcome';
+import {Text, View, TouchableOpacity} from 'react-native';
+import styles from '../styles/App';
 
 export default class App extends Component {
+  state = {
+    slogan: 'Seu canal de nerdologia',
+  }
+
+  alternar = () => {
+    this.setState({
+      slogan: this.state.slogan ? '' : 'Seu canal de nerdologia',
+    });
+  }
+
   render() {
     return (
-      <Welcome titulo='Canal Geek Dev' slogan='Seu canal de nerdologia'/>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this.alternar}>
+          <Text>Mudar state</Text>
+        </TouchableOpacity>
+        <Text>Canal Geek Dev</Text>
+        <Text>{this.state.slogan}</Text>
+      </View>
     );
   }
 }
